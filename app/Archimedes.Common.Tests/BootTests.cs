b81e7bb3c889583e72f739c14adabc29c76736fb
Feodor Fitsner <feodor@appveyor.com>
2015-08-23 21:47:44 -0700

    Read app.config directly in test

--- app/Archimedes.Common.Tests/BootTests.cs
+++ app/Archimedes.Common.Tests/BootTests.cs
@@ -1,9 +1,19 @@
 ﻿namespace Archimedes.Common.Tests
 {
 	using Xunit;
+	using System.Configuration;
 
 	public class BootTests
 	{
+		[Fact]
+		public void Can_Read_App_Config()
+		{
+			// appSettings
+			var someValue = ConfigurationManager.AppSettings["ApplicationName"];
+			Console.WriteLine("SomeValue: " + someValue);
+			Assert.Equal("Archimedes Testing Project", someValue);
+		}
+		
 		[Fact]
 		public void CanBoot()
 		{
